b2c09c9194f7b8a45429eb6001a2b26e80973a04
Irek Sołek <intrek@gmail.com>
2021-01-18 18:07:38 +0100

    Fix ExchangeRateCell mapping

--- CurrencyConverter/Converter/Model/ExchangeRateCellModelMapper.swift
+++ CurrencyConverter/Converter/Model/ExchangeRateCellModelMapper.swift
@@ -10,10 +10,10 @@ protocol ExchangeRateCellModelMapperProtocol {
 
 struct ExchangeRateCellModelMapper: ExchangeRateCellModelMapperProtocol {
   func map(exchangeRate: ExchangeRateModel) -> ExchangeRateCellModel {
-    let title = ExchangeRateCellRowModel(lhsTile: "1 \(exchangeRate.sourceCurrency.name)",
+    let title = ExchangeRateCellRowModel(lhsTile: "1 \(exchangeRate.sourceCurrency.code)",
                                          rhsTitle: NSAttributedString(string: "\(exchangeRate.receiveAmount)"))
     let description = ExchangeRateCellRowModel(lhsTile: "\(exchangeRate.sourceCurrency.name)",
-                                               rhsTitle: NSAttributedString(string: "\(exchangeRate.receiveCurrency.name)"))
+                                               rhsTitle: NSAttributedString(string: "\(exchangeRate.receiveCurrency.name) · \(exchangeRate.receiveCurrency.code)"))
     return ExchangeRateCellModel(title: title, description: description)
   }
 }
