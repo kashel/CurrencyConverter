89e32f81b970193d5444dabfb6fccf8648e827d6
Irek Sołek <intrek@gmail.com>
2021-01-23 11:39:05 +0100

    exchange rate formatter

--- CurrencyConverter/Converter/Model/ExchangeRateCellModelMapper.swift
+++ CurrencyConverter/Converter/Model/ExchangeRateCellModelMapper.swift
@@ -9,9 +9,20 @@ protocol ExchangeRateCellModelMapperProtocol {
 }
 
 struct ExchangeRateCellModelMapper: ExchangeRateCellModelMapperProtocol {
+  let numberFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.locale = Locale.current
+    formatter.numberStyle = .decimal
+    formatter.minimumFractionDigits = 4
+    formatter.maximumFractionDigits = 4
+    formatter.roundingMode = .halfUp
+    return formatter
+  }()
+  
   func map(exchangeRate: ExchangeRateModel) -> ExchangeRateCellModel {
+    let exchangeRateValue = numberFormatter.string(from: exchangeRate.receiveAmount as NSDecimalNumber)
     let title = ExchangeRateCellRowModel(lhsTile: "1 \(exchangeRate.sourceCurrency.code)",
-                                         rhsTitle:"\(exchangeRate.receiveAmount)")
+                                         rhsTitle: exchangeRateValue ?? "")
     let description = ExchangeRateCellRowModel(lhsTile: "\(exchangeRate.sourceCurrency.name)",
                                                rhsTitle: "\(exchangeRate.receiveCurrency.name) · \(exchangeRate.receiveCurrency.code)")
     return ExchangeRateCellModel(title: title, description: description)
